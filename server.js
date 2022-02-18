403bc6d33419c3ad1a72f8c08cae537f9d60c19d
Samer Balee <samerbalee@gmail.com>
2022-02-18 18:19:45 +1100

    Added notes route code

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,11 +12,11 @@ app.use(express.static('public'));
 app.use('/api', api);
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('*', (req, res) =>
